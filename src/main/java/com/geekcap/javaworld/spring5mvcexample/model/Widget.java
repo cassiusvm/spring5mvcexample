c10906a4497524a8a56a9a60390dd8c3036d9641
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2018-12-03 10:14:13 -0200

    Update
    -Add Project Lombok

--- src/main/java/com/geekcap/javaworld/spring5mvcexample/model/Widget.java
+++ src/main/java/com/geekcap/javaworld/spring5mvcexample/model/Widget.java
@@ -5,44 +5,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Widget {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
     private String description;
-
-    public Widget() {
-    }
-
-    public Widget(Long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
